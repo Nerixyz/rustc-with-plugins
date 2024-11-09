738669f1fcfe6da1a9b9852aaf7430a38010b15b
Nerixyz <nerixdev@outlook.de>
2024-11-09 17:38:58 +0100

    fix: more lld stuff

--- build-it.sh
+++ build-it.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")"
 
 ./configure \
     --set llvm.plugins=true \
+    --set rust.lld=false \
+    --set rust.use-lld=external \
     --set target.x86_64-unknown-linux-gnu.llvm-config=/usr/bin/llvm-config-18
 
 ./x.py build
