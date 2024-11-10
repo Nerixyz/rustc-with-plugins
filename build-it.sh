ebee95b768f55c2f7dbcb91bfa0583a1246ddd7d
Nerixyz <nerixdev@outlook.de>
2024-11-10 11:44:02 +0100

    fix: link llvm as shared lib

--- build-it.sh
+++ build-it.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 
 ./configure \
     --set llvm.plugins=true \
+    --set llvm.link-shared=true \
     --set rust.lld=false \
     --set rust.use-lld=false \
     --set target.x86_64-unknown-linux-gnu.llvm-config=/usr/bin/llvm-config-18
